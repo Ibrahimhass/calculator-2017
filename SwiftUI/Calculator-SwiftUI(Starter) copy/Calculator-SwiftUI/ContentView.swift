7dc88d07f235125f3413a40c57df1449ed7ed34e
Md Ibrahim Hassan <mdibrahimhassan@gmail.com>
2022-01-04 16:48:27 +0530

    Added code for Starter code

---
+++ SwiftUI/Calculator-SwiftUI(Starter) copy/Calculator-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Calculator-SwiftUI
+//
+//  Created by Swiftify on 04/01/22.
+//
+
+import SwiftUI
+
+// --------------------------------------------------------------------------------
+// ViewController
+// --------------------------------------------------------------------------------
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
